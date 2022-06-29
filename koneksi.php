cdb75fb09539ff807258231dbbd0864abafdb87b
WhiteHilmi <muhammadhilmithabibi@gmail.com>
2022-06-29 18:38:22 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+$servername="localhost";
+$city="root";
+$message="";
+$dbname = "db_penjualan";
+$koneksi = mysqli_connect($servername, $city, $message, $dbname) or die("Koneksi gagal dibangun");
+
+ ?>
